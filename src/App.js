01ba327238c01b502b3971a85d8fc80f0b913754
subhamkumarmal <subhammal53@gmail.com>
2021-03-05 16:56:49 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+const App = () =>{
+  return(
+    <>
+    <h1 className="text-center">subham kumar mal</h1>
+    <div className="container">
+  <div className="row">
+    <div className="col-sm">
+     <div class="card" >
+  <img class="card-img-top" src="https://picsum.photos/201/300"  height="200px" alt="Card image cap" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     </div>
-  );
+    <div className="col-sm">
+     <div class="card" >
+  <img class="card-img-top" src="https://picsum.photos/202/300"  height="200px" alt="Card image cap"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col-sm">
+     <div class="card" >
+  <img class="card-img-top" src="https://picsum.photos/203/300"  height="200px" alt="Card image cap" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+    </>
+  )
 }
+
 
 export default App;
